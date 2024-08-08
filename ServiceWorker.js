44f1700b7af927b95368978495c398bbd33a59cd
Lymeng (លីម៉េង) <56832985+Lymengchun@users.noreply.github.com>
2024-08-08 15:03:19 +0700

    build 11

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Lymeng-Flappy Bird Kh-1.4";
+const contentToCache = [
+    "Build/build 11.loader.js",
+    "Build/build 11.framework.js.unityweb",
+    "Build/build 11.data.unityweb",
+    "Build/build 11.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
